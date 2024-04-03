cc0fd332862646e34ed6b884de8a1fb4ba47162d
tjsiv <tjsivelsjr@gmail.com>
2024-04-02 23:19:56 -0700

    had to use mock data as api was making straight line graphs

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 async function main() {
-    const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=de72006e67634d1c8f6f3ac2482661a6")
-    const result = await response.json()
+    // const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=de72006e67634d1c8f6f3ac2482661a6")
+    // const result = await response.json()
 
     
     // let GME = result.GME
@@ -9,15 +9,15 @@ async function main() {
     // let BNTX = result.BTNX
     
 
-    // const { GME, MSFT, DIS, BNTX } = mockData;
-    // const stocks = [GME, MSFT, DIS, BNTX];
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
     
     // Bonus Note: 
     // Another way to write the above lines would to refactor it as:
-    const {GME, MSFT, DIS, BNTX} = result 
+    // const {GME, MSFT, DIS, BNTX} = result 
     // This is an example of "destructuring" an object
     // "Destructuring" creates new variables from an object or an array
-    const stocks = [GME, MSFT, DIS, BNTX];
+    // const stocks = [GME, MSFT, DIS, BNTX];
 
     
 
